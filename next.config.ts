c78e1552ded14e6a0d2590dd7dbb006d60140d1b
mario.ferreyra <mario.ferreyra@knotion.com>
2026-01-20 14:06:56 -0600

    fix: redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "sitmorelia.com.mx",
+          },
+        ],
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "www.sitmorelia.com.mx",
+          },
+        ],
+        destination: "/es",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
